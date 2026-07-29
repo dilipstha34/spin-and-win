b61578d8d88b2fc6ceba7975c5158a14d933a5fe
Dilip <dstha0304@gmail.com>
2026-07-29 20:44:08 +0545

    add amount deposit functionality

---
+++ spinNwin.js
@@ -0,0 +1,29 @@
+// 1. Despot some money
+// 2. Determine number of lines to bet on
+// 3. Collect a bet amount
+// 4. Spin the slot machine
+// 5. Check if the player won
+// 6. give the user their winnings
+// 7. Play again
+
+const prompt = require("prompt-sync")();
+
+const deposit = () => {
+    while(true) {
+        const depositAmount = prompt("Enter the amount you want to deposit: ");
+        const numberDepositAmount = parseFloat(depositAmount);
+
+        if(isNaN(numberDepositAmount) || numberDepositAmount <=0)
+        {
+            console.log("Invalid deposit amount, try again");
+        }
+        else
+        {
+            return numberDepositAmount;
+        }
+    }
+    
+};
+
+const depositAmount = deposit();
+console.log(depositAmount);
